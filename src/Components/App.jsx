4340240f8fe2c5120a9e3a0aa8cf1cd0c9e3d772
Peter Mutungi <3sjk0krrr@gmail.com>
2025-04-26 19:49:21 +0300

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,20 @@
 import './App.css';
-import { useState, useEffect } from 'react';  // Import useEffect
+import { useState, useEffect } from 'react'; 
 import Todolist from './Components/Todolist';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  // useEffect hook to send request to JSONPlaceholder API and update the state
   useEffect(() => {
-    // Fetch data from JSONPlaceholder API
     fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.json()) // Convert the response to JSON
+      .then((response) => response.json())
       .then((data) => {
-        setTodos(data); // Update state with the fetched todos
+        setTodos(data); 
       })
       .catch((error) => {
-        console.error('Error fetching data:', error); // Log any error
+        console.error('Error fetching data:', error); 
       });
-  }, []); // Empty dependency array ensures this effect runs once on mount
+  }, []); 
 
   return (
     <div className="App">
