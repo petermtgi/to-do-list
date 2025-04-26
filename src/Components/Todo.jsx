b47846e7545f50717b0b2462ce5f281f32725b96
Peter Mutungi <3sjk0krrr@gmail.com>
2025-04-26 18:51:09 +0300

    update

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = ({ todo }) => {
   return (
     <div>
-      <input type="checkbox" checked={todo.completed} readOnly />
+      <input type="checkbox" checked={todo.completed}  />
       <span>{todo.title}</span>
     </div>
   );
